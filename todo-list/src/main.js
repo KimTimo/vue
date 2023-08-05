b38b190b2840ca146a85c18bc574294be10563b7
kimtimo <eogus8875@naver.com>
2023-08-05 20:30:14 +0900

    주석 추가

--- todo-list/src/main.js
+++ todo-list/src/main.js
@@ -1,4 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+/**
+ * Commit Test
+ */
+
+createApp(App).mount("#app");
