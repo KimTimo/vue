bcf05ea6e2683101ee5dd990c53fad61bfd79863
KimTimo <eogus8875@naver.com>
2023-08-07 17:45:56 +0900

    add push

---
+++ vue3-posts/src/lectures/4/App_CompositionAPI.vue
@@ -0,0 +1,46 @@
+<template>
+	<div>
+		<h2>반응형 메시지</h2>
+		<p>{{ reactiveMessage }}</p>
+		<button v-on:click="addReactiveMessage">Add Message</button>
+		<h2>일반 메시지</h2>
+		<p>{{ nomalMessage }}</p>
+		<button v-on:click="addNormalMessage">Add Message</button>
+	</div>
+</template>
+
+<script>
+import { onMounted, onBeforeMount, ref } from 'vue';
+export default {
+	setup() {
+		console.log('setup()');
+		const reactiveMessage = ref('Hello Reactive Message');
+		const addReactiveMessage = () => {
+			reactiveMessage.value = reactiveMessage.value + '!';
+		};
+		// console.log('isRef(reactiveMessage): ', isRef(reactiveMessage));
+
+		let nomalMessage = 'Hello Nomal Message';
+		const addNormalMessage = () => {
+			nomalMessage = nomalMessage + '!';
+		};
+		// console.log('isRef(nomalMessage): ', isRef(nomalMessage));
+
+		onMounted(() => {
+			console.log('onMounted');
+		});
+		onBeforeMount(() => {
+			console.log('onBeforeMount');
+		});
+
+		return {
+			reactiveMessage,
+			nomalMessage,
+			addReactiveMessage,
+			addNormalMessage,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
