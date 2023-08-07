20a92036292b5b893cb91ed08ad7380248b472a5
KimTimo <eogus8875@naver.com>
2023-08-07 14:01:38 +0900

    주석제거

--- vue3-posts/src/layouts/TheView.vue
+++ vue3-posts/src/layouts/TheView.vue
@@ -1,11 +1,7 @@
 <!-- 싱글파일화 적용 -->
-<!-- 로컬환경에서 Commit Test -->
-<!-- 이메일 다시 적용 -->
 <template>
 	<main>
-		<div class="container py-4">
-			<h2>Hello Vue3</h2>
-		</div>
+		<div class="container py-4"></div>
 	</main>
 </template>
 
