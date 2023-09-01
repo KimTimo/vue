8df62ae3717af0f9159dc74e6b8006839a520213
KimTimo <eogus8875@naver.com>
2023-09-01 14:21:25 +0900

    템플릿추가

--- vue3-posts/src/App.vue
+++ vue3-posts/src/App.vue
@@ -7,6 +7,11 @@
 			{{ message }}
 		</p>
 		<button @click="increment">click</button>
+
+		<div v-bind:title="dynamicTitle">마우스를 올려보세요.</div>
+		<div>
+			<input type="text" v-bind:disabled="isInputDisabled" />
+		</div>
 	</div>
 </template>
 
@@ -15,12 +20,14 @@ import { ref } from 'vue';
 
 export default {
 	setup() {
+		const isInputDisabled = true;
 		const counter = ref(0);
 		const message = ref('Hello Vue3');
 		const increment = () => {
 			counter.value++;
 		};
 		return {
+			isInputDisabled,
 			counter,
 			message,
 			increment,
